3c0a11b9f08c058eb501709994dda19a9fed6754
maciejszulia <maciej.szulia@gmail.com>
2021-05-17 12:12:41 +0200

    Update main.py

--- praca-domowa-8-pandas1/main.py
+++ praca-domowa-8-pandas1/main.py
@@ -24,3 +24,19 @@ print(df[df['Imie'] == 'MACIEJ'])
 # sumę wszystkich urodzonych dzieci w całym danym okresie,
 
 print(df.agg({'Liczba': ['sum']}))
+
+# sumę dzieci urodzonych w latach 2000-2005
+
+print(df[((df.Rok >= 2000) & (df.Rok <= 2005))].agg({'Liczba': ['sum']}))
+
+# sumę urodzonych chłopców i dziewczynek
+print('chlopcy :', df[df.Plec == 'M'].agg({'Liczba': ['sum']}))
+print('dziewczynki :', df[df.Plec == 'K'].agg({'Liczba': ['sum']}))
+
+# najbardziej popularne imię dziewczynki i chłopca w danym roku ( czyli po 2 rekordy na rok),
+print(df.loc[df[df.Plec == 'M'].groupby("Rok")["Liczba"].idxmax()])
+print(df.loc[df[df.Plec == 'K'].groupby("Rok")["Liczba"].idxmax()])
+
+#najbardziej popularne imię dziewczynki i chłopca w całym danym okresie,
+print(df.loc[df[df.Plec == 'M']['Liczba'].idxmax()])
+print(df.loc[df[df.Plec == 'K']['Liczba'].idxmax()])
